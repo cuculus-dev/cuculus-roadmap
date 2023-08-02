4b54b1f568a17ffedd61217a98d3e724d0b85ee6
takecchi <takecchi.kobayashi@gmail.com>
2023-08-02 18:47:19 +0900

    hostedZoneの指定

--- sst.config.ts
+++ sst.config.ts
@@ -1,17 +1,23 @@
-import { SSTConfig } from "sst";
-import { NextjsSite } from "sst/constructs";
+import { SSTConfig } from 'sst';
+import { NextjsSite } from 'sst/constructs';
 
 export default {
   config(_input) {
     return {
-      name: "cuculus-roadmap",
-      region: "ap-northeast-1",
+      name: 'cuculus-roadmap',
+      region: 'ap-northeast-1',
     };
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
-      const site = new NextjsSite(stack, "site",{
-        customDomain: stack.stage == 'prod' ? "roadmap.cuculus.jp" : `${stack.stage}.roadmap.cuculus.jp`,
+      const site = new NextjsSite(stack, 'site', {
+        customDomain: {
+          domainName:
+            stack.stage == 'prod'
+              ? 'roadmap.cuculus.jp'
+              : `${stack.stage}.roadmap.cuculus.jp`,
+          hostedZone: 'cuculus.jp',
+        },
       });
 
       stack.addOutputs({

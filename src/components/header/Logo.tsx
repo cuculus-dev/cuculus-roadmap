3360bb126a39494ccf0a7a95e1d92c69720dbd7e
takecchi <takecchi.kobayashi@gmail.com>
2023-08-04 21:54:23 +0900

    error fix

--- src/components/header/Logo.tsx
+++ src/components/header/Logo.tsx
@@ -9,43 +9,7 @@ type Props = {
 const Logo = ({ height = '60' }: Props) => {
   return (
     <Link href="/">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        height={height}
-        viewBox="0 0 266 74"
-        preserveAspectRatio="xMinYMid"
-      >
-        <text
-          x="12"
-          y="50%"
-          dy="0.4em"
-          dominant-baseline="alphabetic"
-          letter-spacing="11"
-          font-weight="bold"
-          font-size="40"
-          font-family="Meiryo UI"
-          fill="#ffffff"
-          stroke-width="23"
-          stroke="#003300"
-          stroke-linejoin="round"
-        >
-          Cuculus
-        </text>
-        <text
-          x="12"
-          y="50%"
-          dy="0.4em"
-          dominant-baseline="alphabetic"
-          letter-spacing="11"
-          font-weight="bold"
-          font-size="40"
-          font-family="Meiryo UI"
-          fill="#ffffff"
-          stroke-width="0"
-        >
-          Cuculus
-        </text>
-      </svg>
+      Cuculus
     </Link>
   );
 };

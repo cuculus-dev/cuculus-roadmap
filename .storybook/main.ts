76c666426d7a9469dbad24abc5c3d9671bfd558b
takecchi <takecchi.kobayashi@gmail.com>
2023-08-04 11:43:35 +0900

    Aliasの設定

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,19 @@
+const path = require('path');
 import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
+  webpackFinal: async (config) => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          '@': path.resolve(__dirname, '../src'), // 環境によって異なります
+        },
+      },
+    }
+  },
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-links',

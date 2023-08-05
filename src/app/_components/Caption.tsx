b9a57e7c6c8a0b0e376c7c86281dec0c67305178
takecchi <takecchi.kobayashi@gmail.com>
2023-08-06 01:28:04 +0900

    横幅の制限

---
+++ src/app/_components/Caption.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import WidthProvider from '@/app/_components/WidthProvider';
+
+const Caption = () => {
+  return <WidthProvider>TEST</WidthProvider>;
+};
+
+export default Caption;

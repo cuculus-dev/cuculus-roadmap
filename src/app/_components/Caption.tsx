699a5f4de242cc9b928337a33abdb71a0495bec0
takecchi <takecchi.kobayashi@gmail.com>
2023-08-12 10:41:16 +0900

    Captionカラー修正

--- src/app/_components/Caption.tsx
+++ src/app/_components/Caption.tsx
@@ -53,7 +53,7 @@ const Title = styled('h1')`
   }
 
   font-weight: 600;
-  color: #00ced1;
+  color: ${({ theme }) => theme.palette.primary.main};
 `;
 
 const Description = styled('div')`
@@ -62,7 +62,7 @@ const Description = styled('div')`
   margin: 0 auto;
   padding-right: 16px;
   padding-left: 16px;
-  color: ${({ theme }) => theme.palette.primary.main};
+  color: ${({ theme }) => theme.palette.grey[800]};
 
   max-width: 780px;
   font-size: 24px;

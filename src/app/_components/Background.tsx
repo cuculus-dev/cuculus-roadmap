f62de11e1f0cfc94ea997e9ca23f32f4c45389ee
kawanolk <startNN3014@gmail.com>
2023-08-19 01:21:07 +0900

    svgファイル追加。背景画像およびカラー適用

---
+++ src/app/_components/Background.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import { styled } from '@mui/material';
+
+const BackgroundColor = styled('div')`
+  background-color: #dcecfc;
+`;
+
+const StyledZindex1 = styled('div')`
+  background-image: url('/images/cloud_1.svg');
+  background-repeat: no-repeat;
+  background-position: top; //xy軸で px指定する
+  background-size: auto;
+  z-index: 1;
+`;
+
+const StyledZindex2 = styled('div')`
+  background-image: url('/images/cloud_2.svg');
+  background-repeat: no-repeat;
+  background-position: top; //xy軸で px指定する
+  background-size: auto;
+  z-index: 2;
+`;
+
+const StyledZindex3 = styled('div')`
+  background-image: url('/images/cloud_3.svg');
+  background-repeat: no-repeat;
+  background-position: top; //xy軸で px指定する
+  background-size: auto;
+  z-index: 3;
+`;
+
+const StyledZindex4 = styled('div')`
+  background-image: url('/images/cloud_4.svg');
+  background-repeat: no-repeat;
+  background-position: top; //xy軸で px指定する
+  background-size: auto;
+  z-index: 4;
+`;
+
+//svgファイルから画像サイズいじって合わせる
+
+export default function Background({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <BackgroundColor>
+      <StyledZindex1>
+        <StyledZindex2>
+          <StyledZindex3>
+            <StyledZindex4>{children}</StyledZindex4>
+          </StyledZindex3>
+        </StyledZindex2>
+      </StyledZindex1>
+    </BackgroundColor>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Caption from '@/app/_components/Caption';
+import Background from '@/app/_components/Background';
 
 export default function Home() {
   return (
     <main>
-      <Caption />
+      <Background>
+        <Caption />
+        <Caption /> {/*画像サイズ確認用*/}
+      </Background>
     </main>
   );
 }

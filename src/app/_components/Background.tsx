fc649404d38f2dc04792d656d13a96904772d572
kawanolk <startNN3014@gmail.com>
2023-08-19 21:41:27 +0900

    backgroundの修正

--- src/app/_components/Background.tsx
+++ src/app/_components/Background.tsx
@@ -10,36 +10,44 @@ const Back = styled('div')`
   width: 100%;
 `;
 
-const Cloud4 = styled('img')`
-  max-width: 1024px;
+const Cloud1 = styled('img')`
+  max-width: 1255px;
   width: 100%;
-
-  position: absolute;
-  z-index: -10;
-`;
-
-const Cloud2 = styled('img')`
-  max-width: 1024px;
-  width: 100%;
-
-  position: absolute;
-  z-index: -20;
-`;
-
-const Cloud3 = styled('img')`
-  max-width: 1024px;
-  width: 100%;
+  margin-top: -330px;
+  margin-left: -800px;
 
   position: absolute;
   z-index: -30;
 `;
 
-const Cloud1 = styled('img')`
-  max-width: 1024px;
+const Cloud2 = styled('img')`
+  max-width: 1226px;
   width: 100%;
+  margin-top: -40px;
+  margin-left: 400px;
 
   position: absolute;
   z-index: -40;
+`;
+
+const Cloud3 = styled('img')`
+  max-width: 1283px;
+  width: 100%;
+  margin-top: 160px;
+  margin-left: -720px;
+
+  position: absolute;
+  z-index: -10;
+`;
+
+const Cloud4 = styled('img')`
+  max-width: 1006px;
+  width: 100%;
+  margin-top: 320px;
+  margin-left: 750px;
+
+  position: absolute;
+  z-index: -20;
 
   // FIXME タブレット以下だった場合は黄色にして最前面にしちゃうサンプル
   ${({ theme }) => theme.breakpoints.down('tablet')} {
@@ -55,10 +63,10 @@ export default function Background({
   return (
     <>
       <Back>
-        <Cloud4 src="/images/cloud_4.svg" />
+        <Cloud1 src="/images/cloud_1.svg" />
         <Cloud2 src="/images/cloud_2.svg" />
         <Cloud3 src="/images/cloud_3.svg" />
-        <Cloud1 src="/images/cloud_1.svg" />
+        <Cloud4 src="/images/cloud_4.svg" />
       </Back>
       {children}
     </>

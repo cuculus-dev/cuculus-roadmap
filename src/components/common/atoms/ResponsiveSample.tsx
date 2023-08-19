4c91b460c89c4948750f820336aa8b8ca0644e94
takecchi <takecchi.kobayashi@gmail.com>
2023-08-19 19:28:41 +0900

    レスポンシブ対応サンプル

---
+++ src/components/common/atoms/ResponsiveSample.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { styled } from '@mui/material/styles';
+
+const IsDesktop = styled('div')`
+  ${({ theme }) => theme.breakpoints.down('laptop')} {
+    display: none;
+  }
+`;
+
+const IsLaptop = styled('div')`
+  ${({ theme }) => theme.breakpoints.up('laptop')} {
+    display: none;
+  }
+
+  ${({ theme }) => theme.breakpoints.down('tablet')} {
+    display: none;
+  }
+`;
+
+const IsTablet = styled('div')`
+  ${({ theme }) => theme.breakpoints.up('tablet')} {
+    display: none;
+  }
+
+  ${({ theme }) => theme.breakpoints.down('mobile')} {
+    display: none;
+  }
+`;
+
+const IsMobile = styled('div')`
+  ${({ theme }) => theme.breakpoints.up('mobile')} {
+    display: none;
+  }
+`;
+
+export default function ResponsiveSample() {
+  return (
+    <>
+      <IsDesktop>This is desktop.</IsDesktop>
+      <IsLaptop>This is laptop.</IsLaptop>
+      <IsTablet>This is tablet.</IsTablet>
+      <IsMobile>This is mobile.</IsMobile>
+    </>
+  );
+}

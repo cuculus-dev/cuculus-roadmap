28ec61c19185f2d6ed593efdeaec313c7306f3a1
takecchi <takecchi.kobayashi@gmail.com>
2023-08-21 04:21:53 +0900

    諸々調整

--- src/components/common/molecules/GitHubLink.tsx
+++ src/components/common/molecules/GitHubLink.tsx
@@ -1,28 +1,23 @@
 'use client';
 
 import GitHubSvg from '@assets/images/github-mark.svg';
-import Link from 'next/link';
-import { styled } from '@mui/material/styles';
+import { IconButton } from '@mui/material';
 
 type Props = {
   href: string;
 };
 
-const StyledLink = styled(Link)`
-  color: grey;
-
-  :hover,
-  :focus,
-  :active,
-  :focus-visible {
-    color: black;
-  }
-`;
-
 export default function GitHubLink({ href }: Props) {
   return (
-    <StyledLink href={href} passHref>
-      <GitHubSvg />
-    </StyledLink>
+    <IconButton
+      target="_blank"
+      rel="noopener noreferrer"
+      href={href}
+      aria-label="github"
+      title="GitHub"
+      size="small"
+    >
+      <GitHubSvg width="35" height="34" viewBox="0 0 98 96" />
+    </IconButton>
   );
 }
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,37 +1,43 @@
 'use client';
 
 import { styled } from '@mui/material/styles';
-import Link from 'next/link';
 import GitHubLink from '@/components/common/molecules/GitHubLink';
 import CuculusLogo from '@/components/common/molecules/CuculusLogo';
 
 const StyledHeader = styled('header')`
   top: 0;
-  display: flex;
   position: sticky;
-  flex-direction: row;
-  align-items: center;
-  padding: 0 20px;
-  box-shadow: none;
   border-style: solid;
   border-color: ${({ theme }) => theme.palette.grey[100]};
   border-width: 0;
   border-bottom-width: 2px;
   border-bottom-color: #3ea8ff;
-  background-color: rgba(0, 0, 0, 0.0625);
-  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.3);
+  background-color: rgba(255, 255, 255, 0.6);
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.25);
   color: ${({ theme }) => theme.palette.grey[800]};
   z-index: ${({ theme }) => theme.zIndex.appBar};
+`;
+
+const Container = styled('div')`
+  display: flex;
+  max-width: ${({ theme }) => theme.breakpoints.values.desktop}px;
+  min-height: 60px;
+  padding: 0 24px;
+  margin: 0 auto;
+  flex-direction: row;
+  align-items: center;
 `;
 
 const Header = () => {
   return (
     <StyledHeader>
-      <div style={{ padding: '2px' }}>
-        <CuculusLogo href={'/'} />
-      </div>
-      <div style={{ marginLeft: 'auto' }} />
-      <GitHubLink href="https://github.com/cuculus-dev" />
+      <Container>
+        <div style={{ padding: '2px' }}>
+          <CuculusLogo href={'/'} />
+        </div>
+        <div style={{ marginLeft: 'auto' }} />
+        <GitHubLink href="https://github.com/cuculus-dev" />
+      </Container>
     </StyledHeader>
   );
 };

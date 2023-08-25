944cd1e566f22727cd302a70b7f2871537c52735
takecchi <takecchi.kobayashi@gmail.com>
2023-08-26 02:11:52 +0900

    クエリパラメータを使用するように

--- src/swr/client/issues.ts
+++ src/swr/client/issues.ts
@@ -1,20 +1,24 @@
 import useSWRImmutable from 'swr';
 import { components } from '@octokit/openapi-types';
 
-type params = { milestone: number };
+type Params = { milestone: number };
 
 export type issue = components['schemas']['issue'];
 
-const fetcher = async ({ url }: { url: string; params: params }) => {
+const fetcher = async ({ url, params }: { url: string; params: Params }) => {
+  const query = new URLSearchParams();
+  query.append('milestone', params.milestone.toString());
   return (await (
-    await fetch(`${process.env.NEXT_PUBLIC_GITHUB_API_URL}${url}`)
+    await fetch(
+      `${process.env.NEXT_PUBLIC_GITHUB_API_URL}${url}?` + query.toString(),
+    )
   ).json()) as issue[];
 };
 
 export const useIssues = (issueNumber: number) => {
   const { data } = useSWRImmutable<issue[]>(
     {
-      url: '/repos/cuculus-dev/cuculus-community/issues?',
+      url: '/repos/cuculus-dev/cuculus-community/issues',
       params: { milestone: issueNumber },
     },
 

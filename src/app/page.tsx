8090161dd4b51d9c5f5bc776313a37efe26480ad
kawanolk <startNN3014@gmail.com>
2023-08-27 21:13:37 +0900

    カードを新規作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Caption from '@/app/_components/Caption';
+import Card from '@/app/_components/Card';
 import MilestonesSample from '@/app/_components/MilestonesSample';
 
 export default function Home() {
   return (
     <main>
       <Caption />
-      <MilestonesSample />
+      <Card>
+        <MilestonesSample />
+      </Card>
     </main>
   );
 }

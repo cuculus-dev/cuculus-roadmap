fbf55c09e9d0aa1d92a2096f22992d72267de02f
takecchi <takecchi.kobayashi@gmail.com>
2023-09-06 00:46:56 +0900

    エラーの返却

--- src/swr/client/milestones.ts
+++ src/swr/client/milestones.ts
@@ -3,16 +3,19 @@ import { components } from '@octokit/openapi-types';
 
 export type milestone = components['schemas']['milestone'];
 
-const fetcher = async ({ url }: { url: string }) => {
-  return (await (
-    await fetch(`${process.env.NEXT_PUBLIC_GITHUB_API_URL}${url}`)
-  ).json()) as milestone[];
+const fetcher = async ({ url }: { url: string }): Promise<milestone[]> => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_GITHUB_API_URL}${url}`);
+  if (res.ok) {
+    return (await res.json()) as milestone[];
+  } else {
+    throw new Error('An error occurred while fetching the data.');
+  }
 };
 
 export const useMilestones = () => {
-  const { data } = useSWRImmutable<milestone[]>(
+  const { data, error } = useSWRImmutable<milestone[], Error>(
     { url: '/repos/cuculus-dev/cuculus-community/milestones' },
     fetcher,
   );
-  return { data };
+  return { data, error };
 };

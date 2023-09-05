4aa2ffeb82eae481e320ab9f5242a174044e4d04
takecchi <takecchi.kobayashi@gmail.com>
2023-09-05 23:49:45 +0900

    閉じたissueも表示するように

--- src/swr/client/issues.ts
+++ src/swr/client/issues.ts
@@ -1,13 +1,14 @@
 import useSWRImmutable from 'swr';
 import { components } from '@octokit/openapi-types';
 
-type Params = { milestone: number };
+type Params = { milestone: number; state: 'open' | 'close' | 'all' };
 
 export type issue = components['schemas']['issue'];
 
 const fetcher = async ({ url, params }: { url: string; params: Params }) => {
   const query = new URLSearchParams();
   query.append('milestone', params.milestone.toString());
+  query.append('state', params.state.toString());
   return (await (
     await fetch(
       `${process.env.NEXT_PUBLIC_GITHUB_API_URL}${url}?` + query.toString(),
@@ -19,7 +20,7 @@ export const useIssues = (issueNumber: number) => {
   const { data } = useSWRImmutable<issue[]>(
     {
       url: '/repos/cuculus-dev/cuculus-community/issues',
-      params: { milestone: issueNumber },
+      params: { milestone: issueNumber, state: 'all' },
     },
 
     fetcher,

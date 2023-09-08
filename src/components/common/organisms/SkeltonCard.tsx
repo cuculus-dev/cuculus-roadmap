8b9bc4844a8d9168efd7b86ed57bf18e8bc21029
kawanolk <startNN3014@gmail.com>
2023-09-08 22:16:52 +0900

    スケルトンの更新

--- src/components/common/organisms/SkeltonCard.tsx
+++ src/components/common/organisms/SkeltonCard.tsx
@@ -1,11 +1,23 @@
 'use client';
 
-import MuiCard from '@mui/material/Card';
-import { CardContent, CardHeader, Skeleton, Typography } from '@mui/material';
+import {
+  CardContent,
+  CardHeader,
+  Skeleton,
+  Typography,
+  styled,
+} from '@mui/material';
+
+const StyledSkeltonCard = styled('div')`
+  width: 100%;
+  background-color: #ffffff;
+  border-bottom: 1px solid;
+  border-bottom-color: #d9d9d9;
+`;
 
 export default function SkeltonCard() {
   return (
-    <MuiCard sx={{ maxWidth: 345, width: 345 }}>
+    <StyledSkeltonCard>
       <CardHeader
         avatar={<Skeleton variant="circular" width={40} height={40} />}
         title={<Skeleton width="100%" />}
@@ -15,6 +27,6 @@ export default function SkeltonCard() {
           <Skeleton width="100%" />
         </Typography>
       </CardContent>
-    </MuiCard>
+    </StyledSkeltonCard>
   );
 }

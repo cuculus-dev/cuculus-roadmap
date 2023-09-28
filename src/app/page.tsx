e0402f6a7f4bb884c495fb584c3378e82c27ec99
KND-T <t.kondo.up@gmail.com>
2023-09-28 20:45:04 +0900

    余白追加_調整予定

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,17 @@ import Milestones from '@/app/_components/Milestones';
 
 export default function Home() {
   return (
-    <main style={{ display: 'flex' }}>
+    <main
+      style={{
+        display: 'flex',
+        justifyContent: 'space-evenly',
+        marginLeft: '5%',
+        marginRight: '5%',
+        width: '100%',
+      }}
+    >
       <Caption />
-      <div style={{ display: 'row' }}>
+      <div style={{ display: 'row', width: '111%', padding: '1%' }}>
         <Milestones />
       </div>
     </main>
